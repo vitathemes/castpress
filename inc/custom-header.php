c9d138fe6631dcb521b241806e7963b5087b9fa4
Nima Pourmohamadi <nimaprmdi@gmail.com>
2021-11-23 11:51:24 +0330

    Add pluggable funtions

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -1,6 +1,6 @@
 <?php
 /**
-  * Sample implementation of the Custom Header feature 
+  *  Sample implementation of the Custom Header feature 
   *
   *
   *  @link https://developer.wordpress.org/themes/functionality/custom-headers/
@@ -8,26 +8,27 @@
   *  @package castpress
 */
 
-/**
-* Add your custom logo to the login page
-*/
-function castpress_filter_login_head() {
-    if ( has_custom_logo() ) :
-        $image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
-    ?>
+if ( ! function_exists( 'castpress_filter_login_head' )) {
+    /**
+    * Add your custom logo to the login page
+    */
+    function castpress_filter_login_head() {
+        if ( has_custom_logo() ) :
+            $image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
+        ?>
 
-    <style type="text/css">
-        .login h1 a {
-            background-image: url(<?php echo esc_url($image[0]);?>);
-            -webkit-background-size: <?php echo absint($image[1]) ?>px;
-            background-size: <?php echo absint($image[1]) ?>px; 
-            height: <?php echo absint($image[2]) ?>px;
-            width: <?php echo absint($image[1]) ?>px;
-        }
-    </style>
+        <style type="text/css">
+            .login h1 a {
+                background-image: url(<?php echo esc_url($image[0]);?>);
+                -webkit-background-size: <?php echo absint($image[1]) ?>px;
+                background-size: <?php echo absint($image[1]) ?>px; 
+                height: <?php echo absint($image[2]) ?>px;
+                width: <?php echo absint($image[1]) ?>px;
+            }
+        </style>
 
-    <?php
-    endif;
+        <?php
+        endif;
+    }
+    add_action( 'login_head', 'castpress_filter_login_head', 100 );
 }
-
-add_action( 'login_head', 'castpress_filter_login_head', 100 );
--- inc/setup.php
+++ inc/setup.php
@@ -87,14 +87,17 @@ if ( ! function_exists( 'castpress_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'castpress_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function castpress_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'castpress_content_width', 640 );
+
+if ( ! function_exists( 'castpress_content_width' )) {
+	/**
+	 * Set the content width in pixels, based on the theme's design and stylesheet.
+	 *
+	 * Priority 0 to make it available to lower priority callbacks.
+	 *
+	 * @global int $content_width
+	 */
+	function castpress_content_width() {
+		$GLOBALS['content_width'] = apply_filters( 'castpress_content_width', 640 );
+	}
+	add_action( 'after_setup_theme', 'castpress_content_width', 0 );
 }
-add_action( 'after_setup_theme', 'castpress_content_width', 0 );
